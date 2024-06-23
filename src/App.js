959dadecf39067aee6f55fb67041880a36d8c2af
Hyunjun Lee <leehyunjune11@gmail.com>
2024-06-23 17:12:54 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Test
         </a>
       </header>
     </div>
